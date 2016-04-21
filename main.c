78a42f792522b8d98113fbd80adb447c70a5defe
urthbound <jeffowler@gmail.com>
2016-04-20 20:15:50 -0400

    init

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* --------*/
+/* utility */
+/* --------*/
+
+void hello_world() {
+    printf("hello world");
+}
+
+/* -----------*/
+/* executable */
+/* -----------*/
+
+int main() {
+    hello_world();
+    printf("\n");
+    return 0;
+}
+
